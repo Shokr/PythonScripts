aa61a1c641828bafa0659d4831f5e992a191d9ac
Restyled.io <commits@restyled.io>
2020-11-13 20:16:28 +0000

    Restyled by isort

--- Bots/telegram/main.py
+++ Bots/telegram/main.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import CallbackContext, CommandHandler, Updater
 
 
 def hello(update: Update, context: CallbackContext) -> None:
