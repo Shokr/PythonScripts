861820a870b36588b29c68dd43cf809944e9a3aa
Shokr <mohammedshokr2014@gmail.com>
2020-11-13 22:08:05 +0200

    13112020

---
+++ Bots/telegram/main.py
@@ -0,0 +1,21 @@
+from datetime import date
+
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+
+def hello(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+def time(update: Update, context: CallbackContext):
+    update.message.reply_text(f'Today: {date.today()}')
+
+
+updater = Updater('1315138805:AAGSVl4y0a5HS8addQRBgSq-QPeC00QyM7Y')
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('today', time))
+
+updater.start_polling()
+updater.idle()
