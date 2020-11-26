eb5f7357f97cfce3da3fc04852430f42553fa162
shokr <mohammedshokr2014@gmail.com>
2020-11-26 15:30:33 +0200

    VFX

---
+++ VFX/Image_Tuning/ChangeImageBG.py
@@ -0,0 +1,10 @@
+from pixellib.tune_bg import alter_bg
+
+change_bg = alter_bg()
+print(change_bg)
+
+change_bg.load_pascalvoc_model("deeplabv3_xception_tf_dim_ordering_tf_kernels.h5")
+print('-=-=-')
+change_bg.change_bg_img(f_image_path="img/sample.jpg", b_image_path="img/background.jpg",
+                        output_image_name="img/new_img.jpg")
+print('opopop')
