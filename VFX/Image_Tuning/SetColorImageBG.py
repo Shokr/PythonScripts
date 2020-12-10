4a5734ad117ec8218c46976644b63a443a5a2a22
Restyled.io <commits@restyled.io>
2020-12-10 15:14:22 +0000

    Restyled by yapf

--- VFX/Image_Tuning/SetColorImageBG.py
+++ VFX/Image_Tuning/SetColorImageBG.py
@@ -2,7 +2,8 @@ import pixellib
 from pixellib.tune_bg import alter_bg
 
 change_bg = alter_bg()
-change_bg.load_pascalvoc_model("deeplabv3_xception_tf_dim_ordering_tf_kernels.h5")
-change_bg.color_bg(
-    "img/sample.jpg", colors=(0, 128, 0), output_image_name="colored_bg.jpg"
-)
+change_bg.load_pascalvoc_model(
+    "deeplabv3_xception_tf_dim_ordering_tf_kernels.h5")
+change_bg.color_bg("img/sample.jpg",
+                   colors=(0, 128, 0),
+                   output_image_name="colored_bg.jpg")
