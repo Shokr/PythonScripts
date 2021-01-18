32070c44cc056539ae4d9f6dd123a1e699a7113a
Restyled.io <commits@restyled.io>
2021-01-18 17:48:20 +0000

    Restyled by black

--- VFX/Image_Tuning/ChangeImageBG.py
+++ VFX/Image_Tuning/ChangeImageBG.py
@@ -3,8 +3,7 @@ from pixellib.tune_bg import alter_bg
 change_bg = alter_bg()
 print(change_bg)
 
-change_bg.load_pascalvoc_model(
-    "deeplabv3_xception_tf_dim_ordering_tf_kernels.h5")
+change_bg.load_pascalvoc_model("deeplabv3_xception_tf_dim_ordering_tf_kernels.h5")
 print("-=-=-")
 change_bg.change_bg_img(
     f_image_path="img/sample.jpg",
