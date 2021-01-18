51cac52576ebce5fe628bd7fb0c1ce041675074d
Restyled.io <commits@restyled.io>
2021-01-18 17:45:12 +0000

    Restyled by yapf

--- Automation/windowShot/windowShot.py
+++ Automation/windowShot/windowShot.py
@@ -16,13 +16,8 @@ windowList = CGWindowListCopyWindowInfo(
 systemWindows = ["SystemUIServer", "Window Server", "Spotlight"]
 searchname = argv[1].lower()
 for i in windowList:
-    if (
-        searchname in i["kCGWindowOwnerName"].lower()
-        and i["kCGWindowOwnerName"] not in systemWindows
-    ):
+    if (searchname in i["kCGWindowOwnerName"].lower()
+            and i["kCGWindowOwnerName"] not in systemWindows):
         print(i["kCGWindowNumber"], i["kCGWindowOwnerName"])
-        os.system(
-            'screencapture -l{0} "tmp/{0}{1}.png"'.format(
-                i["kCGWindowNumber"], i["kCGWindowOwnerName"]
-            )
-        )
+        os.system('screencapture -l{0} "tmp/{0}{1}.png"'.format(
+            i["kCGWindowNumber"], i["kCGWindowOwnerName"]))
