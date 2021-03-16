9d693649567d363b21b61237bfcafbbbbd192cb2
muhammed <mohammedshokr2014@gmail.com>
2021-03-16 13:33:14 +0200

    16/02/2021

---
+++ GUI/Automate_Chat/main.py
@@ -0,0 +1,17 @@
+"""
+Automate any Chat-Messenger with Python
+Easily automate Whatsapp | Facebook | Instagram Chat-Messenger with Python using PyAutoGUI
+"""
+
+import pyautogui
+import time
+
+time.sleep(5)
+
+text = 'I Love You'
+
+while True:
+    pyautogui.typewrite(text)
+
+    time.sleep(1)
+    pyautogui.press('enter')
